a69f5ee6cf1730d3a0d0c2ec1cac4b6982906eaa
Aj <btchaggler@gmail.com>
2014-12-22 23:59:05 +0000

    test function. asserts to ensure list sorted. insertion sort works

--- basic_sorts.py
+++ basic_sorts.py
@@ -1,9 +1,36 @@
-def insert_sort(arr):
+def insert_sort_inplace(arr):
 
     arrlen = len(arr)
 
     for i in range(1, arrlen):
-        cmpindex = i - 1
+        for j in range(0, i):
+            if arr[i] < arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
 
-        if arr[cmpindex] > arr[i]:
+    return arr
 
+
+def sorting_test(fn):
+    rndlist = produce_test_input()
+    rndlist = fn(rndlist)
+
+    for i in range(1, len(rndlist)):
+        if rndlist[i] < rndlist[i-1]:
+            print "Error at Index: ", i
+            return False
+
+    return True
+
+
+def produce_test_input():
+    import random
+    return random.sample(xrange(100), 15)
+
+def main():
+    rndlist = produce_test_input()
+    print rndlist
+    insert_sort_inplace(rndlist)
+    print rndlist
+    assert sorting_test(insert_sort_inplace)
+
+main()
