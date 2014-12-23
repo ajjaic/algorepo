15a3cf3c6b537e22a7a6e9dbcc5235789bc0f740
Aj <btchaggler@gmail.com>
2014-12-23 01:31:04 +0000

    added selection sort. added doc strings for sorting functions

--- basic_sorts.py
+++ basic_sorts.py
@@ -1,4 +1,12 @@
 def insert_sort_inplace(arr):
+    """
+    Sort all the elements in a list in place.
+    As the elements are picked off from one end
+    of the list, they are put in sorted order,
+    at the other end.
+
+    BIG(O):
+    """
 
     arrlen = len(arr)
 
@@ -10,6 +18,14 @@ def insert_sort_inplace(arr):
     return arr
 
 def insert_sort_inplace_variation(arr):
+    """
+    Same as 'insert_sort_inplace'. Difference
+    being that, newly inserted elements are
+    inserted from the end of the sorted part of
+    the list.
+
+    BIG(O):
+    """
     arrlen = len(arr)
 
     for i in range(1, arrlen):
@@ -28,13 +44,41 @@ def insert_sort_inplace_variation(arr):
 
 #A useless algorithm.
 #Not as bad as bogosort and bozosort though
-def bubble_sort(arr):
+def bubble_sort_inplace(arr):
+    """
+    Traverse the list as many times as the
+    number of elements. In each traversal,
+    the largest element bubbles up to the top.
+
+    BIG(O): N^2
+    """
     arrlen = len(arr)
 
     for i in range(arrlen-1, 0, -1):
         for j in range(0, i):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+
+    return arr
+
+def selection_sort_inplace(arr):
+    """
+    Every iteration through the list, find
+    the smallest element and swap it with
+    the last index in the sorted part of the
+    list.
+
+    BIG(O): N^2
+    """
+
+    arrlen = len(arr)
+
+    for i in range(0, arrlen - 1):
+        cm = i
+        for j in range(i+1, arrlen):
+            if arr[j] < arr[cm]:
+                cm = j
+        arr[i], arr[cm] = arr[cm], arr[i]
 
     return arr
 
@@ -49,7 +93,6 @@ def sorting_test(fn):
 
     return True
 
-
 def produce_test_input():
     import random
     return random.sample(xrange(100), 15)
@@ -57,6 +100,7 @@ def produce_test_input():
 def main():
     assert sorting_test(insert_sort_inplace)
     assert sorting_test(insert_sort_inplace_variation)
-    assert sorting_test(bubble_sort)
+    assert sorting_test(bubble_sort_inplace)
+    assert sorting_test(selection_sort_inplace)
 
 main()
